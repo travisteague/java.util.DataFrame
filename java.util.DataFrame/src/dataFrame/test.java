0f59d0e9c5d2da0e82df2c41ee88fdeac9e71a05
Travis Teague <travis14teague@gmail.com>
2021-02-25 18:20:49 -0600

    New tests

--- java.util.DataFrame/src/dataFrame/test.java
+++ java.util.DataFrame/src/dataFrame/test.java
@@ -1,6 +1,7 @@
 package dataFrame;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class test {
@@ -8,19 +9,46 @@ public class test {
 	public static void main(String[] args) {
 		List<Integer> col1 = new ArrayList<Integer>();
 		List<Integer> col2 = new ArrayList<Integer>();
+		List<Integer> col3 = new ArrayList<Integer>();
+		List<Integer> col4 = new ArrayList<Integer>();
+		List<Integer> col5 = new ArrayList<Integer>();
+		List<Integer> col6 = new ArrayList<Integer>();
 		ArrayList<String> colNames = new ArrayList<String>();
 		
-		for(int i = 0; i < 5; i++) {
-			col1.add(i);
+		for(int i = 0; i < 10; i++) {
+			if(i <= 4) {
+				col1.add(i);
+			}else {
+				col2.add(i);
+			}
 		}
-		for(int i = 5; i < 10; i++) {
-			col2.add(i);
+		for(int i = 10; i < 20; i++) {
+			if(i <= 14) {
+				col3.add(i);
+			}else {
+				col4.add(i);
+			}
 		}
+		for(int i = 20; i < 30; i++) {
+			if(i <= 24) {
+				col5.add(i);
+			}else {
+				col6.add(i);
+			}
+		}
+
+
 		colNames.add("One");
 		colNames.add("Two");
-		
-		DataFrame table = new DataFrame(col1, col2, colNames);
+		colNames.add("Three");
+		colNames.add("Four");
+		colNames.add("Five");
+		colNames.add("Six");
+
+		DataFrame table = new DataFrame(colNames, col1, col2, col3, col4, col5, col6);
 		table.head();
+		
+		
 		
 
 	}
