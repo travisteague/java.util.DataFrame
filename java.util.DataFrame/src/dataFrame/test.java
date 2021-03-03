2edbcf6992d10ac9a9cec422a7555441d4b8dcb1
Travis Teague <travis14teague@gmail.com>
2021-03-03 08:57:54 -0600

    Additional code to include String and Double columns

--- java.util.DataFrame/src/dataFrame/test.java
+++ java.util.DataFrame/src/dataFrame/test.java
@@ -8,21 +8,22 @@ public class test {
 
 	public static void main(String[] args) {
 		List<Integer> col1 = new ArrayList<Integer>();
-		List<Integer> col2 = new ArrayList<Integer>();
+		List<Double> col2 = new ArrayList<Double>();
 		List<Integer> col3 = new ArrayList<Integer>();
 		List<Integer> col4 = new ArrayList<Integer>();
 		List<Integer> col5 = new ArrayList<Integer>();
 		List<Integer> col6 = new ArrayList<Integer>();
+		List<String> colString = new ArrayList<String>();
 		ArrayList<String> colNames = new ArrayList<String>();
 		
 		for(int i = 0; i < 10; i++) {
 			if(i <= 4) {
 				col1.add(i);
 			}else {
-				col2.add(i);
+				col2.add((double) i);
 			}
 		}
-		for(int i = 10; i < 20; i++) {
+		/*for(int i = 10; i < 20; i++) {
 			if(i <= 14) {
 				col3.add(i);
 			}else {
@@ -35,17 +36,22 @@ public class test {
 			}else {
 				col6.add(i);
 			}
-		}
+		}*/
 
-
+		colString.add("Row 1");
+		colString.add("Row 2");
+		colString.add("Row 3");
+		colString.add("Row 4");
+		colString.add("Row 5");
+		
 		colNames.add("One");
 		colNames.add("Two");
 		colNames.add("Three");
-		colNames.add("Four");
-		colNames.add("Five");
-		colNames.add("Six");
+		//colNames.add("Four");
+		//colNames.add("Five");
+		//colNames.add("Six");
 
-		DataFrame table = new DataFrame(colNames, col1, col2, col3, col4, col5, col6);
+		DataFrame table = new DataFrame(col1, col2, colString);
 		table.head();
 		
 		
